fc5f4e870da57fd830a63042b680fac9d4a5f277
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-13 15:26:57 -0400

    create parser for input

--- langchain_course/pycode/main.py
+++ langchain_course/pycode/main.py
@@ -3,18 +3,25 @@ from dotenv import load_dotenv
 from langchain.llms import openai
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+import argparse
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
+
 
 llm = openai.OpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
 
 code_prompt = PromptTemplate(template="Write a very short {language} function that will {task}", input_variables=["language", "task"])
 
 code_chain = LLMChain(llm=llm, prompt=code_prompt)
-\
+
 result = code_chain({
-    "language": "python",
-    "task": "return a list of numbers"
+    "language": args.language,
+    "task": args.task
 })
 
 print(result["text"])
