de4ed387b2be29cb92cd284b8cdc992369d8d7b1
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-13 15:21:05 -0400

    use chain to create content

--- langchain_course/pycode/main.py
+++ langchain_course/pycode/main.py
@@ -1,4 +1,20 @@
 import os
 from dotenv import load_dotenv
+from langchain.llms import openai
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
-print(os.getenv('OPENAI_KEY'))
+load_dotenv()
+
+llm = openai.OpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
+
+code_prompt = PromptTemplate(template="Write a very short {language} function that will {task}", input_variables=["language", "task"])
+
+code_chain = LLMChain(llm=llm, prompt=code_prompt)
+\
+result = code_chain({
+    "language": "python",
+    "task": "return a list of numbers"
+})
+
+print(result["text"])
