5c38dfc2ffce6dc7730bce9255cd25979773aca9
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-14 12:08:38 -0400

    add memory to current chat

--- langchain_course/tchat/main.py
+++ langchain_course/tchat/main.py
@@ -1,27 +1,32 @@
 from dotenv import load_dotenv
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
+    input_variables=["content", "messages"],
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True: 
     content = input(">> ")
-
     result = chain({
         "content": content
     }) 
