283f4a2c8de2451783b89de9a507a497f6813aa9
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-14 10:55:51 -0400

    add gitignore

---
+++ langchain_course/tchat/main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt
+)
+
+while True: 
+    content = input(">> ")
+
+    result = chain({
+        "content": content
+    }) 
+
+    print(result["text"])
+
