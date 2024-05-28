dfe1297b3d3a70a441ac0c81e01af404217c2ce3
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-28 16:09:05 -0400

    update descriptions and args_schema. Create report gen tool

--- langchain_course/agents/main.py
+++ langchain_course/agents/main.py
@@ -5,19 +5,30 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder
 )
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
 prompt = ChatPromptTemplate(
-    messages=[HumanMessagePromptTemplate.from_template("{input}"),
-    MessagesPlaceholder(variable_name="agent_scratchpad")]
+    messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}."
+            "Do not make any assumptions about what tables exist or what columns exist. Instead, use the 'describe_tables' function."
+            )), 
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
+    ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -31,4 +42,4 @@ agent_executor = AgentExecutor(
     tools=tools
 )
 
-agent_executor("How many users have provided a shipping address?")
+agent_executor("Summarize the top five most popular products. Write the results to a report file. Include product name, id, price and quantity. Style with basic CSS.")
