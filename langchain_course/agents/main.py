543d05a1b877456ecdf6497c7601b3287d92b09e
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-29 11:18:34 -0400

    create callback tool to log messages to ai

--- langchain_course/agents/main.py
+++ langchain_course/agents/main.py
@@ -7,13 +7,18 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 
-chat = ChatOpenAI()
+handler = ChatModelStartHandler()
+chat = ChatOpenAI(
+    callbacks=[handler]
+)
 
 tables = list_tables()
 prompt = ChatPromptTemplate(
@@ -23,11 +28,13 @@ prompt = ChatPromptTemplate(
             f"The database has tables of: {tables}."
             "Do not make any assumptions about what tables exist or what columns exist. Instead, use the 'describe_tables' function."
             )), 
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
@@ -38,8 +45,15 @@ agent = OpenAIFunctionsAgent(
 
 agent_executor = AgentExecutor(
     agent=agent,
-    verbose=True,
-    tools=tools
+    # verbose=True,
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("Summarize the top five most popular products. Write the results to a report file. Include product name, id, price and quantity. Style with basic CSS.")
+agent_executor(
+    "How many orders are there? Write the results to an html report."
+)
+
+agent_executor(
+    "Repeat the exact same process for users."
+)
